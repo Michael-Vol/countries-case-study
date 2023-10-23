cbbdbf9c316ff2e4b2fea91f3fe580f000b96440
Michael-Vol <mivolakis@gmail.com>
2023-10-23 16:08:09 +0300

    first commit

---
+++ src/main/java/com/example/countriescasestudy/CountriesCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.countriescasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countriescasestudy/CountriesCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countriescasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
