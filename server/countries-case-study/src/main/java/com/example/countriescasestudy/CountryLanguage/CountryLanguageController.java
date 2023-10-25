9eeabc6b0f7c902b4fa8a247dd3ff79ca640af96
Michael-Vol <mivolakis@gmail.com>
2023-10-25 22:21:35 +0300

    feat: finished task 2

--- server/countries-case-study/src/main/java/com/example/countriescasestudy/CountryLanguage/CountryLanguageController.java
+++ server/countries-case-study/src/main/java/com/example/countriescasestudy/CountryLanguage/CountryLanguageController.java
@@ -7,16 +7,14 @@ import com.example.countriescasestudy.Language.Language;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @RequestMapping("/country-languages")
 public class CountryLanguageController {
     private final CountryLanguageService countryLanguageService;
@@ -32,6 +30,7 @@ public class CountryLanguageController {
         return new ResponseEntity<>(GetLanguagesOfCountryResponse.builder()
                 .languages(languages)
                 .countryId(countryId)
+                .countryName(country.getName())
                 .build(), HttpStatus.OK);
     }
 }
--- server/countries-case-study/src/main/java/com/example/countriescasestudy/CountryStats/CountryStatsController.java
+++ server/countries-case-study/src/main/java/com/example/countriescasestudy/CountryStats/CountryStatsController.java
@@ -5,6 +5,7 @@ import com.example.countriescasestudy.CountryStats.dto.CountryStatsDto;
 import com.example.countriescasestudy.CountryStats.dto.GetAllCountryStatsResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 @RequestMapping("/country-stats")
 public class CountryStatsController {
     private final CountryStatsService countryStatsService;
