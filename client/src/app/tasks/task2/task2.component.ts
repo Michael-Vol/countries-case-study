78563c8922716acfb13704ff59b5793bef4d0c23
Michael-Vol <mivolakis@gmail.com>
2023-10-25 22:21:29 +0300

    feat: finished task 2

--- client/src/app/tasks/task2/task2.component.ts
+++ client/src/app/tasks/task2/task2.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {Router} from "@angular/router";
+
+export interface Stats {
+  id: number,
+  name: string,
+  countryCode3: string,
+  year: number,
+  population: number,
+  gdp: number,
+  ratio: number
+}
 
 @Component({
   selector: 'app-task2',
   templateUrl: './task2.component.html',
   styleUrls: ['./task2.component.css']
 })
-export class Task2Component {
+export class Task2Component implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'countryCode3',
+    'year',
+    'population',
+    'gdp',
+    'ratio'
+  ];
+  stats: Stats[] = [];
 
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<Stats>(this.stats);
+
+  constructor(private _httpClient: HttpClient, private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    const res = this._httpClient.get('http://localhost:8080/country-stats')
+    res.subscribe((data: any) => {
+      if ('countryStats' in data) {
+        console.log(data);
+        this.stats = data.countryStats;
+        this.dataSource.data = this.stats;
+      }
+    });
+  }
 }
